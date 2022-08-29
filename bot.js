4b2ad7a402be4eb99c911c569d309da25fe689fb
Pavel Demin <paveldemin@me.com>
2022-08-29 15:48:21 +0800

    feat: first bot feature

---
+++ bot.js
@@ -0,0 +1,37 @@
+// import packages
+const dotenv = require("dotenv");
+const TelegramBot = require("node-telegram-bot-api");
+
+// read env variables
+dotenv.config();
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+const welcomeMessage = (userName) => {
+  return (
+    `🇨🇦 <strong>Welcome, ${userName}!</strong>` +
+    "\n\n" +
+    "This is a telegram bot to track Canadian Immigration Scores." +
+    "\n\n" +
+    "Click <strong>Subscribe to updates</strong> so that the bot can send you latest updates as soon as they are available." +
+    "\n\n" +
+    "You can also check <strong>last CRS score</strong> or last 10 scores." +
+    "\n\n" +
+    "We wish your immigration to succeed soon! 🍁"
+  );
+};
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, welcomeMessage(msg.from.first_name), {
+    parse_mode: "HTML",
+    reply_markup: {
+      keyboard: [
+        ["Subscribe to updates"],
+        ["Check last CRS score"],
+        ["Check previous 10 scores"],
+      ],
+    },
+  });
+});
