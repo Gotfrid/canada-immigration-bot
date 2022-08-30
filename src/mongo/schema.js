3211b7486bcc6a770ee3646896047078ed0035f6
Pavel Demin <paveldemin@me.com>
2022-08-30 12:04:39 +0800

    feat: new data processing

---
+++ src/mongo/schema.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+// Schema definition
+const roundSchema = new Schema({
+  drawNumber: {
+    type: String, // draw number cannot be a Number bc of 91a and 91b
+    index: true,
+    unique: true,
+  },
+  drawDate: String, // Probably should be Date, but string is OK
+  drawName: String,
+  drawSize: Number,
+  drawCRS: Number,
+});
+
+const subscriberSchema = new Schema({
+  chatID: Number,
+  firstName: String,
+  lastName: String,
+  subscribedAt: Date,
+});
+
+// Model definition
+const Round = model("Round", roundSchema);
+const Subscriber = model("subscriber", subscriberSchema);
+
+module.exports = { Round, Subscriber };
