034eeb4a5d70ceedf9a6c9b15b8b0c5fe2a2cdd7
Pavel Demin <paveldemin@me.com>
2022-09-01 11:34:11 +0800

    refactor: good looking lambda function :D

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
 const { fetchDataAndUpdate } = require("./aws/fetchDataAndUpdate");
 
 if (process.env.MODE === "stage") {
+  require("dotenv").config({ path: `${__dirname}/config/.env` });
   (async () => {
-    fetchDataAndUpdate().then((response) => {
+    fetchDataAndUpdate(process.env.STAGE_MONGO_URI).then((response) => {
       console.log(response);
       process.exit(0);
     });
@@ -14,7 +15,11 @@ if (process.env.MODE === "stage") {
 }
 
 exports.handler = async (event) => {
-  const response = await fetchDataAndUpdate();
+  const mongo_uri =
+    event.mode === "stage"
+      ? process.env.STAGE_MONGO_URI
+      : process.env.PROD_MONGO_URI;
+  const response = await fetchDataAndUpdate(mongo_uri);
   console.log(response);
   return await response;
 };
