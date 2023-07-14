ba8d12fe8b5005ea8aed78db9a9cab9bdfd96118
Pavel Demin <paveldemin@me.com>
2023-07-14 17:00:11 +0200

    fix: correct sorting for the latest distribution

--- src/publicHandlers.js
+++ src/publicHandlers.js
@@ -113,7 +113,7 @@ const changeHandler = async (bot, change, groupIds) => {
 const distributionHandler = async (bot, msg) => {
   console.info("Received `distribution` command from", msg.chat.id);
   const crsDocument = await Distribution.find()
-    .sort({ drawDate: -1 })
+    .sort((a, b) => a.drawNumber.localeCompare(b.drawNumber))
     .limit(1)
     .exec();
   const message = distributionMessage(crsDocument[0]);
