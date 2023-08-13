f84e37b50c45b8ce3333790bd7df1662ece83352
Pavel Demin <paveldemin@me.com>
2023-08-13 22:03:04 +0200

    refactor: migrate away from mongoose in the bot code

--- src/bot/publicHandlers.js
+++ src/bot/publicHandlers.js
@@ -7,94 +7,84 @@ const {
   dashboardMessage,
 } = require("./messages");
 const { Round, Subscriber, User, Distribution } = require("../database/schema");
+const {
+  createUser,
+  createSubscriber,
+  removeSubscriber,
+  getLastDraw,
+  getLastNDraws,
+  getLastDistribution,
+  getAllSubscriberIds,
+} = require("../database/mongoFunctions");
 
 const startHandler = async (bot, msg) => {
-  // It makes sense to reset user in the DB when they `start`
-  await User.findOneAndRemove({ chatID: msg.chat.id });
-  await Subscriber.findOneAndRemove({ chatID: msg.chat.id });
-
-  const newUser = new User({
-    chatID: msg.chat.id,
-    firstName: msg.from.first_name,
-    lastName: msg.from.last_name,
-    startedAt: new Date(msg.date * 1000),
-  });
-
-  await newUser.save();
-
-  bot.sendMessage(msg.chat.id, welcomeMessage(msg.from.first_name), {
-    parse_mode: "HTML",
-  });
+  console.info("Received `start` command from", msg.chat.id);
+  await createUser(msg.chat.id, msg.from.first_name, msg.from.last_name, new Date(msg.date * 1000));
+  bot.sendMessage(msg.chat.id, welcomeMessage(msg.from.first_name), { parse_mode: "HTML" });
 };
 
+// FIXME: either remove or move to adminHandlers
 const testHandler = async (bot, msg) => {
   console.info("Received `test` command from", msg.chat.id);
-  await bot.sendMessage(msg.chat.id, "bot is working");
+  await bot.sendMessage(msg.chat.id, "IT'S ALIVE!");
 };
 
 const subscribeHandler = async (bot, msg) => {
   console.info("Received `subscribe` command from", msg.chat.id);
-  const subscriber = await Subscriber.findOne({ chatID: msg.chat.id });
-  if (subscriber === null) {
-    const newSubscriber = new Subscriber({
-      chatID: msg.chat.id,
-      firstName: msg.from.first_name,
-      lastName: msg.from.last_name,
-      subscribedAt: new Date(msg.date * 1000),
-    });
-    await newSubscriber.save();
+  const isSubscribed = createSubscriber(
+    msg.chat.id,
+    msg.from.first_name,
+    msg.from.last_name,
+    new Date(msg.date * 1000),
+  );
+
+  if (isSubscribed) {
     bot.sendMessage(
       msg.chat.id,
-      "You are now subscribed!\nIf you want to stop receiving notifications, you can /unsubscribe."
+      "You are now subscribed!\nIf you want to stop receiving notifications, you can /unsubscribe.",
     );
   } else {
-    bot.sendMessage(
-      msg.chat.id,
-      "You are already subscribed!\nIf you want, you can /unsubscribe."
-    );
+    bot.sendMessage(msg.chat.id, "You are already subscribed!\nIf you want, you can /unsubscribe.");
   }
 };
 
 const unsubscribeHandler = async (bot, msg) => {
   console.info("Received `unsubscribe` command from", msg.chat.id);
-  const subscriber = await Subscriber.findOne({ chatId: msg.chat.id });
-  if (subscriber === null) {
+  const isUnsubscribed = removeSubscriber(msg.chat.id);
+  if (isUnsubscribed) {
     await bot.sendMessage(
       msg.chat.id,
-      "You are not subscribed!\nIf you want to receive notifications, you can /subscribe."
+      "You have unsubscribed.\nIf you want to receive notifications again, you can /subscribe.",
     );
   } else {
-    await Subscriber.findOneAndRemove({ chatID: msg.chat.id });
     await bot.sendMessage(
       msg.chat.id,
-      "You have unsubscribed.\nIf you want to receive notifications again, you can /subscribe."
+      "You are not subscribed!\nIf you want to receive notifications, you can /subscribe.",
     );
   }
 };
 
 const lastHandler = async (bot, msg) => {
   console.info("Received `last` command from", msg.chat.id);
-  const crsDocument = await Round.find().sort({ drawDate: -1 }).limit(1).exec();
-  const message = lastRoundMessage(crsDocument[0]);
+  // const crsDocument = await Round.find().sort({ drawDate: -1 }).limit(1).exec();
+  const draw = await getLastDraw();
+  const message = lastRoundMessage(draw);
   await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
 };
 
 const last50Handler = async (bot, msg) => {
   console.info("Received `last50` command from", msg.chat.id);
-  const crsDocument = await Round.find()
-    .sort({ drawDate: -1 })
-    .limit(50)
-    .exec();
-  const message = last50Message(crsDocument);
+  const data = await getLastNDraws(50);
+  const message = last50Message(data);
   await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
 };
 
+// TODO: make sure it works with new mongo driver
 const changeHandler = async (bot, change, groupIds) => {
   if (change.operationType !== "insert") return;
 
   const message = lastRoundMessage(change.fullDocument, true);
-  const subscribers = await Subscriber.find().select("chatID");
-  const subscriberIds = subscribers.map((sub) => sub.chatID);
+  const subscribers = await getAllSubscriberIds();
 
   // First, send message to the group(s) - they are the priority
   [...groupIds, ...subscriberIds].forEach(async (chatID) => {
@@ -114,11 +104,8 @@ const changeHandler = async (bot, change, groupIds) => {
 
 const distributionHandler = async (bot, msg) => {
   console.info("Received `distribution` command from", msg.chat.id);
-  const crsDocument = await Distribution.find()
-    .sort({ drawDate: -1 })
-    .limit(1)
-    .exec();
-  const message = distributionMessage(crsDocument[0]);
+  const distribution = await getLastDistribution();
+  const message = distributionMessage(distribution);
   await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
 };
 
@@ -131,7 +118,6 @@ const aboutHandler = async (bot, msg) => {
 const dashboardHandler = async (bot, msg) => {
   console.info("Received `dashboard` command from", msg.chat.id);
   const message = dashboardMessage();
-  console.log(message);
   await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
 };
 
---
+++ src/database/mongoFunctions.js
@@ -0,0 +1,80 @@
+const dotenv = require("dotenv");
+const client = require("./mongoClient");
+
+dotenv.config();
+
+const MONGO_DB = process.env.MONGO_DB ?? "test";
+
+const db = client.db(MONGO_DB);
+
+const users = db.collection("users");
+const subscribers = db.collection("subscribers");
+const rounds = db.collection("rounds");
+const distributions = db.collection("distributions");
+
+const createUser = async (chatID, firstName, lastName, startedAt) => {
+  await Promise.all([users.deleteOne({ chatID }), subscribers.deleteOne({ chatID })]);
+  await users.insertOne({ chatID, firstName, lastName, startedAt });
+};
+
+const createSubscriber = async (chatID, firstName, lastName, subscribedAt) => {
+  const oldSubscriber = await subscribers.findOne({ chatID });
+  if (oldSubscriber !== null) {
+    return false;
+  }
+  const newSubscriber = { chatID, firstName, lastName, subscribedAt };
+  await subscribers.insertOne(newSubscriber);
+  return true;
+};
+
+const removeSubscriber = async (chatID) => {
+  const subscriber = await subscribers.findOne({ chatID });
+  if (subscriber === null) {
+    return false;
+  }
+  await subscribers.deleteOne({ chatID });
+  return true;
+};
+
+const getLastDraw = async () => {
+  const filter = {};
+  const options = { sort: { drawDate: -1 } };
+  return await rounds.findOne(filter, options);
+};
+
+const getLastNDraws = async (n) => {
+  const filter = {};
+  const options = { sort: { drawDate: -1 }, limit: n };
+  return await rounds.find(filter, options).toArray();
+};
+
+const getLastDistribution = async () => {
+  const filter = {};
+  const options = { sort: { createdAt: -1 } };
+  return await distributions.findOne(filter, options);
+};
+
+const getInternalStats = async () => {
+  const totalRounds = await rounds.countDocuments();
+  const totalUsers = await users.countDocuments();
+  const totalSubscribers = await subscribers.countDocuments();
+  return { totalRounds, totalUsers, totalSubscribers };
+};
+
+const getAllSubscriberIds = async () => {
+  const filter = {};
+  const options = { projection: { chatID: 1, _id: 0 } };
+  const ids = await subscribers.find(filter, options).toArray();
+  return ids.map((subscriber) => subscriber.chatID);
+};
+
+module.exports = {
+  createUser,
+  createSubscriber,
+  removeSubscriber,
+  getLastDraw,
+  getLastNDraws,
+  getLastDistribution,
+  getAllSubscriberIds,
+  getInternalStats,
+};
