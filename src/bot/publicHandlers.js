f462a6ff74692952b430d4c9d6b7eca5b446a070
Pavel Demin <paveldemin@me.com>
2023-08-21 21:57:53 +0200

    docs: document public handlers

--- src/bot/publicHandlers.js
+++ src/bot/publicHandlers.js
@@ -1,3 +1,6 @@
+const TelegramBot = require("node-telegram-bot-api");
+const { ChangeStreamDocument } = require("mongodb");
+
 const {
   welcomeMessage,
   lastRoundMessage,
@@ -22,12 +25,22 @@ const {
 } = require("../database/mongoFunctions");
 const { GROUP_CHAT_IDS } = require("../config");
 
+/**
+ * Handles `/start` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
 const startHandler = async (msg) => {
   console.info("Received `start` command from", msg.chat.id);
   await createUser(msg.chat.id, msg.from.first_name, msg.from.last_name, new Date(msg.date * 1000));
   await bot.sendMessage(msg.chat.id, welcomeMessage(msg.from.first_name), { parse_mode: "HTML" });
 };
 
+/**
+ * Handles `/subscribe` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
 const subscribeHandler = async (msg) => {
   console.info("Received `subscribe` command from", msg.chat.id);
   const isSubscribed = await createSubscriber(
@@ -44,6 +57,11 @@ const subscribeHandler = async (msg) => {
   }
 };
 
+/**
+ * Handles `/unsubscribe` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
 const unsubscribeHandler = async (msg) => {
   console.info("Received `unsubscribe` command from", msg.chat.id);
   const isUnsubscribed = await removeSubscriber(msg.chat.id);
@@ -54,6 +72,11 @@ const unsubscribeHandler = async (msg) => {
   }
 };
 
+/**
+ * Handles `/last` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
 const lastHandler = async (msg) => {
   console.info("Received `last` command from", msg.chat.id);
   // const crsDocument = await Round.find().sort({ drawDate: -1 }).limit(1).exec();
@@ -62,6 +85,11 @@ const lastHandler = async (msg) => {
   await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
 };
 
+/**
+ * Handles `/last50` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
 const last50Handler = async (msg) => {
   console.info("Received `last50` command from", msg.chat.id);
   const data = await getLastNDraws(50);
@@ -69,6 +97,45 @@ const last50Handler = async (msg) => {
   await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
 };
 
+/**
+ * Handles `/distribution` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
+const distributionHandler = async (msg) => {
+  console.info("Received `distribution` command from", msg.chat.id);
+  const distribution = await getLastDistribution();
+  const message = distributionMessage(distribution);
+  await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
+};
+
+/**
+ * Handles `/about` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
+const aboutHandler = async (msg) => {
+  console.info("Received `about` command from", msg.chat.id);
+  const message = aboutMessage();
+  await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
+};
+
+/**
+ * Handles `/dashboard` command
+ * @param {TelegramBot.Message} msg - The message object containing information about the user and the message.
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
+const dashboardHandler = async (msg) => {
+  console.info("Received `dashboard` command from", msg.chat.id);
+  const message = dashboardMessage();
+  await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
+};
+
+/**
+ * Handles change event in the MongoDB
+ * @param {ChangeStreamDocument} change - Newly added document to the MongoDB
+ * @returns {Promise<void>} - A Promise that resolves when the message has been sent to the user.
+ */
 const changeHandler = async (change) => {
   if (process.env.MODE === "test") return;
   if (change.operationType !== "insert") return;
@@ -90,25 +157,6 @@ const changeHandler = async (change) => {
       console.error("Message could not be sent.");
     }
   }
-};
-
-const distributionHandler = async (msg) => {
-  console.info("Received `distribution` command from", msg.chat.id);
-  const distribution = await getLastDistribution();
-  const message = distributionMessage(distribution);
-  await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
-};
-
-const aboutHandler = async (msg) => {
-  console.info("Received `about` command from", msg.chat.id);
-  const message = aboutMessage();
-  await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
-};
-
-const dashboardHandler = async (msg) => {
-  console.info("Received `dashboard` command from", msg.chat.id);
-  const message = dashboardMessage();
-  await bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
 };
 
 exports.startHandler = startHandler;
