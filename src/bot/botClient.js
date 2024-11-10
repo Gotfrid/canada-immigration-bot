7445520b5adb531ebc7d388ee1dfee806888da31
Pavel Demin <paveldemin@me.com>
2024-11-10 15:02:28 +0000

    chore: reset project

--- src/bot/botClient.js
+++
@@ -1,6 +0,0 @@
-const TelegramBot = require("node-telegram-bot-api");
-const { BOT_TOKEN } = require("../config");
-
-const bot = new TelegramBot(BOT_TOKEN ?? "", { polling: true });
-
-module.exports = bot;
